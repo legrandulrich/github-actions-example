66caea1836b89424c867b293da4fd4b7c3f18520
legrandulrich <legrandulrich@gmail.com>
2025-04-09 00:19:30 -0400

    code

---
+++ src/main/java/org/oufon/githubcicdactions/GithubCicdActionsApplication.java
@@ -0,0 +1,21 @@
+package org.oufon.githubcicdactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to Github CI/CD Actions!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCicdActionsApplication.class, args);
+    }
+
+}
